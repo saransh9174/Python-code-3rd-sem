f86070908281aed446763945e95bd8769967927c
saransh9174 <70794556+saransh9174@users.noreply.github.com>
2022-08-29 22:05:16 +0530

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -72,3 +72,22 @@ for i in range(0, rows):
 # Print the second part of the diamond
 for i in reversed(range(0, rows - 1)):
     print(diamond[i])
+###########MATRIX########
+# importing numpy as np
+import numpy as np
+
+
+# creating first matrix
+A = np.array([[1, 2], [3, 4]])
+
+# creating second matrix
+B = np.array([[4, 5], [6, 7]])
+
+print("Printing elements of first matrix")
+print(A)
+print("Printing elements of second matrix")
+print(B)
+
+# adding two matrix
+print("Addition of two matrix")
+print(np.add(A, B))
